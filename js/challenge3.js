6aafd8fd863f011b20ced2fb029ff1d27c744929
klillios <97644198+klillios@users.noreply.github.com>
2024-04-10 09:52:32 -0400

    Add files via upload

---
+++ js/challenge3.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", function() {
+    //alert("hello!");
+    let elements = document.querySelectorAll("figcaption");
+    
+    let caption = elements[1]
+    console.log(caption.innerHTML);
+    alert(caption.innerHTML);
+})
